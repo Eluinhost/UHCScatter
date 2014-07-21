4be2426cc557f5306f85cc356f2d3ba60e65130d
Graham <graham_howden1@yahoo.co.uk>
2014-07-21 13:05:55 +0100

    pass scatter command to it's own executor

---
+++ src/main/java/com/publicuhc/uhcscatter/ScatterCommand.java
@@ -0,0 +1,14 @@
+package com.publicuhc.uhcscatter;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ScatterCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        //TODO parse input and do stuff
+        return false;
+    }
+}
--- src/main/java/com/publicuhc/uhcscatter/UhcScatterPlugin.java
+++ src/main/java/com/publicuhc/uhcscatter/UhcScatterPlugin.java
@@ -4,4 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class UhcScatterPlugin extends JavaPlugin {
 
+    @Override
+    public void onEnable()
+    {
+        getServer().getPluginCommand("scatter").setExecutor(new ScatterCommand());
+    }
 }
