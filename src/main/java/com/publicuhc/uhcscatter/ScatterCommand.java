531b99189697a9a40b44160170fddbe8cd708172
Graham <graham_howden1@yahoo.co.uk>
2014-07-21 21:25:03 +0100

    unused import

--- src/main/java/com/publicuhc/uhcscatter/ScatterCommand.java
+++ src/main/java/com/publicuhc/uhcscatter/ScatterCommand.java
@@ -14,7 +14,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
 
 import java.util.*;
