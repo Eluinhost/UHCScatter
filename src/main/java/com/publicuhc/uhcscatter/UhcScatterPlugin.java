bffe3478c1643b86da98d6a1c1eda992b8512415
Graham <graham_howden1@yahoo.co.uk>
2014-08-17 18:33:59 +0100

    use frameworkjavaplugin

--- src/main/java/com/publicuhc/uhcscatter/UhcScatterPlugin.java
+++ src/main/java/com/publicuhc/uhcscatter/UhcScatterPlugin.java
@@ -1,5 +1,8 @@
 package com.publicuhc.uhcscatter;
 
+import com.publicuhc.pluginframework.FrameworkJavaPlugin;
+import com.publicuhc.pluginframework.configuration.Configurator;
+import com.publicuhc.pluginframework.shaded.inject.Inject;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,26 +10,28 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UhcScatterPlugin extends JavaPlugin {
+public class UhcScatterPlugin extends FrameworkJavaPlugin {
+
+    private FileConfiguration config;
+
+    @Inject
+    public void setConfigurator(Configurator configs)
+    {
+        config = configs.getConfig("main");
+    }
 
     @Override
-    public void onEnable()
+    public void onFrameworkEnable()
     {
-        FileConfiguration config = getConfig();
-        config.options().copyDefaults(true);
-        saveConfig();
-
         List<String> stringMats = config.getStringList("allowed blocks");
         List<Material> mats = new ArrayList<Material>();
         for(String stringMat : stringMats) {
             Material mat = Material.matchMaterial(stringMat);
-            if(null == mat) {
+            if(null == mat)
                 getLogger().severe("Unknown material " + stringMat);
-            } else {
+            else
                 mats.add(mat);
-            }
         }
-
         int maxAttempts = config.getInt("max attempts per location");
 
         getServer().getPluginCommand("sct").setExecutor(new ScatterCommand(mats, maxAttempts));
